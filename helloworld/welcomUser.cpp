ad5e8ac11ac79403d03c1663061ff93892255ba4
cc <2908493772@qq.com>
2025-09-17 11:15:53 +0800

    hellowolrd

---
+++ helloworld/welcomUser.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+
+// 函数定义
+void welcomUser( string name){
+    cout<<"Hello," <<name << endl <<  "  Welocom to Cpp World!" << endl;
+}
+
+/*
+int main(){
+    string name;
+    cout << "Please input your name:" << endl;
+    cin>>name;
+    welcomUser(name);
+    return 0;
+}
+*/
