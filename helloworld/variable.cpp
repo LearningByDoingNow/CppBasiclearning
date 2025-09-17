e54becd1448348c5705d7ee6337eddcff4982cba
cc <2908493772@qq.com>
2025-09-17 11:27:32 +0800

    add file: variable.cpp

---
+++ helloworld/variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    string name;
+    name = "I love the University Chinese Adademy of Science";
+    cout << name << endl;
+    cout << "loop print more seconds ::" << endl;
+    for(int i = 0 ; i < 5 ;i++){
+        cout << name << endl;
+    }
+    return 0;
+}
