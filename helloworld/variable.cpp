f79a4e8abe4d7eaa1978f44fdc82c1c8a38bbcf9
cc <2908493772@qq.com>
2025-09-17 11:30:34 +0800

    add file: variable.cpp

--- helloworld/variable.cpp
+++ helloworld/variable.cpp
@@ -2,8 +2,10 @@
 #include <string>
 using namespace std;
 
-int main(){
+string name  = "global variable";
+int age;  // 不能在函数体外进行单独赋值 比如  int age; age = 18; 这样是错误的  但是可以 int age = 18; 这样是正确的，前者可以在函数体内实现
 
+int main(){
     string name;
     name = "I love the University Chinese Adademy of Science";
     cout << name << endl;
